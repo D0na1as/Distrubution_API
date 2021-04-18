b382b6976f8a57112db548d2aa452248bbe30c13
donatas <d.kadakauskis@gmail.com>
2021-04-18 18:50:53 +0300

    commit

--- src/main/java/Distribution/API/base/Exceptions/ExceptionHandling.java
+++ src/main/java/Distribution/API/base/Exceptions/ExceptionHandling.java
@@ -17,25 +17,4 @@ public class ExceptionHandling extends ResponseEntityExceptionHandler{
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<>("Bad Request", HttpStatus.BAD_REQUEST);
     }
-
-    //For Heroku, port is not constant
-    @Bean
-    public TomcatServletWebServerFactory tomcatCustomizer() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers((connector -> {
-
-            /**
-             * Gets PORT env variable and sets it as tomcat server port.
-             * In Heroku env, the PORT env variable must be used by tomcat
-             * because if not the process will fail.
-             * Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
-             */
-            String envPort = System.getenv("PORT");
-            // If PORT is NULL, we'll use the default port
-            if (envPort != null) {
-                connector.setPort(Integer.parseInt(envPort));
-            }
-        }));
-        return factory;
-    }
 }
